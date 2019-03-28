e9fce99badfdb7aac4150de3ec930618342f4348
Srinivas Anand <aforanand22@outlook.com>
2019-03-29 00:59:38 +0530

    Passing method reference between components and also showd two ways of callling method reference

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ class App extends Component {
     ]
   }
 
-swithNameHandler = () =>{
+swithNameHandler = (nameNew) =>{
   console.log("clicked");
   //Dont do this
   //this.state.persons[0].name = 'nananana'
   this.setState({persons: 
     [
-      { name : 'New name ',age:28 },
+      { name : nameNew,age:28 },
       { name : 'Manu',age:23 }
     ]
   })
@@ -26,9 +26,14 @@ swithNameHandler = () =>{
     return (
       <div className="App">
         <h1>Hello World</h1>
-        <button onClick={this.swithNameHandler}>Swith name</button>
-        <Person name={this.state.persons[0].name} age = {this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age ={this.state.persons[1].age}>My hobbies: Racing</Person>
+        <button onClick={() => this.swithNameHandler('nenw name!!')}>Swith name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age = {this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age ={this.state.persons[1].age}
+          click = {this.swithNameHandler.bind(this,"newName!")}>My hobbies: Racing</Person>
       </div>
     );
   }
