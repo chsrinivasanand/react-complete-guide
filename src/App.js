3ff135b957c188a3e1ce4f2a4a17e136877baaa3
Srinivas Anand <aforanand22@outlook.com>
2019-03-29 00:29:53 +0530

    Hooks usages

--- src/App.js
+++ src/App.js
@@ -1,38 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
+const app = props => {
+  const [personsState, setPersonState] = useState(
+    {
+      persons: [
+        { name : 'Max',age:28 },
+        { name : 'Manu',age:29 }
+      ]
 
-  state = {
-    persons: [
-      { name : 'Max',age:28 },
-      { name : 'Manu',age:29 }
-    ]
-  }
+    }
+  );
 
-swithNameHandler = () =>{
-  console.log("clicked");
-  //Dont do this
-  //this.state.persons[0].name = 'nananana'
-  this.setState({persons: 
-    [
-      { name : 'New name ',age:28 },
-      { name : 'Manu',age:23 }
-    ]
-  })
-}
-  render() {
-    return (
+
+  //Keeping multiple states we need this because in hooks state will be overridden with new values
+  //it wont keep old values
+  //so its better to keep different state objects so that each state can be updated with new values whenever we required .
+  const [otherState,setOtherState] = useState('some string');
+    console.log(personsState,otherState);
+  
+    const swithNameHandler = () =>{
+    console.log("clicked");
+    // in reacthooks will not keep old onjects which are presnt in the state
+    //whelre as in components state will be apended i.e old values wills kept as it is in new state along with updated values
+    setPersonState({persons: 
+      [
+        { name : 'New name ',age:28 },
+        { name : 'Manu',age:23 }
+      ]
+    })
+    };
+
+  return (
       <div className="App">
         <h1>Hello World</h1>
-        <button onClick={this.swithNameHandler}>Swith name</button>
-        <Person name={this.state.persons[0].name} age = {this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age ={this.state.persons[1].age}>My hobbies: Racing</Person>
+        <button onClick={swithNameHandler}>Swith name</button>
+        <Person name={personsState.persons[0].name} age = {personsState.persons[0].age}/>
+        <Person name={personsState.persons[1].name} age ={personsState.persons[1].age}>My hobbies: Racing</Person>
       </div>
     );
-  }
 }
 
-export default App;
+export default app;
+
+
