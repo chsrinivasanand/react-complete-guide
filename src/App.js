2c78f3ac04624511ec1eceb5dd502ac32f904378
Srinivas Anand <aforanand22@outlook.com>
2019-05-05 16:25:36 +0530

    Adding two way binding

--- src/App.js
+++ src/App.js
@@ -22,6 +22,17 @@ swithNameHandler = (nameNew) =>{
     ]
   })
 }
+
+nameChangedHandler = (event) => {
+    this.setState({persons:
+            [
+                { name : 'Max',age:32 },
+                { name : event.target.value,age:33 }
+            ]
+    })
+}
+
+
   render() {
     return (
       <div className="App">
@@ -33,7 +44,8 @@ swithNameHandler = (nameNew) =>{
         <Person 
           name={this.state.persons[1].name} 
           age ={this.state.persons[1].age}
-          click = {this.swithNameHandler.bind(this,"newName!")}>My hobbies: Racing</Person>
+          click = {this.swithNameHandler.bind(this,"newName!")}
+          changed = {this.nameChangedHandler} >My hobbies: Racing</Person>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,7 +5,9 @@ const person = (props) => {
         <div>
         <p onClick={props.click}>I'm {props.name} and I am {props.age} year old </p>
         <p>{props.children}</p>
-        </div>
+
+        <input type="text" onChange={props.changed} value = {props.name} />
+          </div>
     )
 };
  
