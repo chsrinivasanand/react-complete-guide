9512293a12349dc47c8128105527103571a96329
Srinivas Anand <aforanand22@outlook.com>
2019-05-11 12:05:17 +0530

    Raised sample error

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,8 +2,15 @@ import React from 'react' ;
 import cssClasses from './Person.css' ;
 const person = (props) => {
 
+    const rnd = Math.random();
+
+    if (rnd > 0.7)
+    {
+        throw new Error('Something went wrong');
+    }
 
     return (
+
         <div className= {cssClasses.Person} >
         <p onClick={props.click}>I'm {props.name} and I am {props.age} year old </p>
         <p>{props.children}</p>
